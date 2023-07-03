e529b42d19b44ea68c1419194f6a5bdea0ba930f
Baltss <baltazaralmiron@gmail.com>
2023-07-03 15:20:56 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css'
+
+export function Footer () {
+    return (
+        <div className="main">
+            <p className="nombres">Almirón Baltazar - Gramajo Correa Ayllén</p>
+            <p className="comision">Comisión 56</p>
+            <p className="materia">Programación 3</p>
+            <p className="profesor">Profesor: Orellana Benjamin</p>
+
+        </div>
+
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header () {
+    return (
+        <div>
+            <h2 className="text-center mt-5 mb-5">Trailer Popular Movies</h2>
+        </div>
+    )
+}
